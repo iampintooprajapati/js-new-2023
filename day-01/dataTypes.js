7c5602b993ff8396313137d9d598b11b43f728cc
iampintooprajapti <iampintooprajapati@gmail.com>
2023-10-25 19:22:40 +0530

    dataType

---
+++ day-01/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version ..( no need to declare)
+
+// alert(10+23) // we are using nodejs , not using browser
+
+console.log("Pintoo");
+
+let nameMy = "Pintoo"
+let age = 15
+let isLog = true
+
+/*  */
+// number 
+// bigint
+// string => "" ( recommend ) 
+// boolean  => true or false 
+// null => standalone value 
+// undefined => not defined 
+// symbol => unique 
+
+// object 
+
+console.log(typeof "Pintoo");
+
+console.log(typeof age)
+console.log(typeof isLog)
+console.log(typeof nameMy);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
