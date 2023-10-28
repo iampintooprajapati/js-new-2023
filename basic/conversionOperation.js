42517a14d416da163b36c7875eb82c080dbc77bd
iampintooprajapti <iampintooprajapati@gmail.com>
2023-10-28 17:15:33 +0530

    data types notes

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -2,31 +2,31 @@ let age = 124
 
 let newAge = "100" 
 
-console.log(typeof age);
+// console.log(typeof age);
 
-console.log(typeof(age));
+// console.log(typeof(age));
 
-console.log(typeof newAge);
+// console.log(typeof newAge);
 
 let valueInNumber = Number(newAge)
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 let oldAge = "100abc"
 
 let valueInNumberNew = Number(oldAge)
 
-console.log(typeof valueInNumberNew);
+// console.log(typeof valueInNumberNew);
 
-console.log(valueInNumberNew);
+// console.log(valueInNumberNew);
 
 let coldAge = null
 
 let valueInNumberColdAge = Number(coldAge)
 
-console.log(typeof valueInNumberColdAge);
+// console.log(typeof valueInNumberColdAge);
 
-console.log(valueInNumberColdAge);
+// console.log(valueInNumberColdAge);
 
 // "100" => 100 Number op
 // "100pintoo" => NaN op type is still number confusing
@@ -36,7 +36,7 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // isLoggedIn = 1 => true op 
 // isLoggedIn = 0 => false op
@@ -50,8 +50,67 @@ let newNumber = 101
 
 let stringNumber = String(newNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
+
+// ***************** Operations ***********************//
+
+let value = 102
+
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2%3);
+
+let str1 = "hello"
+
+let str2 = " Pintoo"
+
+let str = str1 + str2
+
+// console.log(str);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log(1 + 2 + 2);
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true)
+// console.log(+"");
+
+let playerRank = 12
+
+playerRank++;
+
+// console.log(playerRank);
+
+// ***************** Comparison  ***********************//
+
+// console.log("2" > 1);
+
+// console.log("02" > 1);
+
+// console.log("2" > 200);
+
+// console.log(null > 0);
+
+// console.log(null == 0);
+
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+
+// console.log(undefined == 0);
+
+// console.log(undefined >= 0);
+
+// === 
+
+console.log("202" === 202);
 
 
---
+++ basic/dataTypesNotes.js
@@ -0,0 +1,50 @@
+
+
+
+// Primitive datatypes
+
+// As per below catagories Primitive datatypes (around 7) : Call by value 
+
+// String , number , boolean , null , undefined , Symbol , BigInt 
+
+const rank = 101
+
+const rankValue = 102.0
+
+const isFirst = false
+
+const ausWin = null
+
+let winWc2023;
+
+const id = Symbol('123')
+
+const rankId = Symbol('123')
+
+console.log(id === rankId);
+
+// const bigNumber = 74185596542214863
+
+// Reference (Non Primitive datatypes)
+
+// Array :  
+
+const socialMediaApps = ["Facebook" , "X" , "Instagram" , "Koo"]
+
+// Objects :
+
+let myObject = {
+    name: "Pintoo",
+    age:"121"
+
+}
+
+// Functions : 
+
+const myAge = function(){
+    console.log("18");
+}
+
+console.log(typeof socialMediaApps);
+    
+
