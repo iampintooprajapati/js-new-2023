5d84c6f352ec8a4dc86ceb8069fafc6e7f37f995
iampintooprajapti <iampintooprajapati@gmail.com>
2023-10-29 13:02:41 +0530

    math and numbers

---
+++ basic/numbersMaths.js
@@ -0,0 +1,53 @@
+const rank = 4500
+
+// console.log(rank);
+
+const newRank = new Number(4500)
+
+// console.log(newRank);
+
+// console.log(newRank.toString().length);
+
+// console.log(newRank.toFixed(2));
+
+const anotherNumber = 355.20
+
+// console.log(anotherNumber.toPrecision(4));
+
+const number = 50000000000
+
+// console.log(`one day my bank balance maybe ${number.toLocaleString('en-IN')}`);
+
+// ******************** Maths **********************************************
+
+// console.log(Math);
+
+// console.log(Math.abs(-200));
+
+// console.log(Math.round(200.369));
+
+// console.log(Math.ceil(200.369));
+
+// console.log(Math.floor(200.369));
+
+// console.log(Math.min(4,5,5,7,1,5,2));
+
+// console.log(Math.max(4,5,5,7,1,5,2));
+
+// console.log(Math.random());
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min =10 
+
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
